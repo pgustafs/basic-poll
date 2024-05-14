d4e0815b36d44affe1193b5076ffc0ab255b616a
pgustafs <pgustafs@redhat.com>
2024-05-14 19:55:30 +0200

    test

--- base/settings.py
+++ base/settings.py
@@ -28,7 +28,7 @@ DEBUG = True
 #ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS", default='127.0.0.1').split(" ")
 # Note! add more strict rules like for example [".apps.ocp.pgustafs.com"]
 ALLOWED_HOSTS = ["*"]
-CSRF_TRUSTED_ORIGINS = ["https://*"]
+CSRF_TRUSTED_ORIGINS = ["https://*.apps.ocp.pgustafs.com"]
 
 # Application definition
 
